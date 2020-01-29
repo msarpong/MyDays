4d1ebcec5b59df59e8eab53f9656d77ee7bb68d1
Marcello Sarpong <msarpong1991@gmail.com>
2020-01-29 18:59:09 +0100

    Fix database

--- app/src/main/java/com/msarpong/mydays/ui/add/text/AddTextScreen.kt
+++ app/src/main/java/com/msarpong/mydays/ui/add/text/AddTextScreen.kt
@@ -2,12 +2,21 @@ package com.msarpong.mydays.ui.add.text
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.msarpong.mydays.R
 
 class AddTextScreen : AppCompatActivity() {
 
+    private lateinit var saveBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_text_screen)
+
+        setupView()
+    }
+
+    private fun setupView() {
+
     }
 }
